12b4f36b7db28485710434e4c562c2971bf02c59
mgweaver <masongweaver@gmail.com>
2021-07-12 23:29:05 -0600

    move mongo uri to env

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+const MONGO_URI = process.env.MONGO_URI
 
 mongoose.set('debug', true)
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb+srv://devuser:LMmGCy3ULVDogMfg@project2db.pvz6f.mongodb.net/project2db?retryWrites=true&w=majority');
+mongoose.connect(MONGO_URI);
 
 module.exports.User = require('./user')
 module.exports.Poll = require('./poll')
